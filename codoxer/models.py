640595a25de3a427fffd5a19866966723a5d99f8
Frederik Laubisch <kafkaese@gmail.com>
2020-09-01 10:30:33 +0200

    implement load cnn and tfidf

--- codoxer/models.py
+++ codoxer/models.py
@@ -1,8 +1,17 @@
 import joblib
-
+from pathlib import Path
+import tensorflow as tf
 
 def load_tfidf():
-    return joblib.load('models/tfidf.pkl')
+    path = Path(__file__).parent / 'models/tfidf.pkl'
+    return joblib.load(path)
 
 def load_cnn():
-    pass
+    path = Path(__file__).parent / 'models/trained_model'
+    return tf.keras.models.load_model(path)
+
+
+'''
+if __name__ == '__main__':
+    load_cnn()
+'''
