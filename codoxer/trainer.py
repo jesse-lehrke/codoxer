2b1820211bee2ad3a2ee1fcdc3d01a61c4f813ac
Frederik Laubisch <kafkaese@gmail.com>
2020-09-01 15:05:18 +0200

    CodoxerModel class skeleton

---
+++ codoxer/trainer.py
@@ -0,0 +1,18 @@
+from codoxer.transformers import CxxTokenizer
+
+from codoxer.cnn_model import CNN_model
+
+
+class CodoxerModel(BaseEstimator):
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def predict_proba(self, X):
+        pass
