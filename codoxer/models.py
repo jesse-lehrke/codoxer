602fe5d5b70b7e141401ac1d37861d857af2c0be
Frederik Laubisch <kafkaese@gmail.com>
2020-09-01 11:22:16 +0200

    Bugfix load_tfidf()

--- codoxer/models.py
+++ codoxer/models.py
@@ -2,16 +2,19 @@ import joblib
 from pathlib import Path
 import tensorflow as tf
 
+class Tfidf(object):
+    pass
+
+
 def load_tfidf():
-    path = Path(__file__).parent / 'models/tfidf.pkl'
+    path = Path(__file__).parent / 'models/tfidf'
     return joblib.load(path)
 
 def load_cnn():
     path = Path(__file__).parent / 'models/trained_model'
     return tf.keras.models.load_model(path)
 
-
 '''
 if __name__ == '__main__':
-    load_cnn()
+    load_tfidf()
 '''
