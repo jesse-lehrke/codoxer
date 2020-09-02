c9a4c12a2f74747e4c38089e2d02e6b2fe887f6d
Frederik Laubisch <kafkaese@gmail.com>
2020-09-02 10:31:33 +0200

    add load_dict

--- codoxer/models.py
+++ codoxer/models.py
@@ -1,3 +1,4 @@
+import json
 import joblib
 from pathlib import Path
 import tensorflow as tf
@@ -7,18 +8,22 @@ class Tfidf(object):
 
 
 def load_tfidf():
-    path = Path(__file__).parent / 'models/tfidf'
+    path = Path(__file__).parent / 'models/vectorizer_132'
     return joblib.load(path)
 
 def load_selector():
-    path = Path(__file__).parent / 'models/selector'
+    path = Path(__file__).parent / 'models/selector_132'
     return joblib.load(path)
 
 def load_cnn():
     path = Path(__file__).parent / 'models/trained_model'
     return tf.keras.models.load_model(path)
 
-'''
+def load_dict():
+    path = Path(__file__).parent / 'models/user_id_dict_132.json'
+    with open(path) as file:
+        return json.load(file)
+
+
 if __name__ == '__main__':
-    load_tfidf()
-'''
+    print(load_dict())
